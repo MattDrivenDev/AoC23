94d7c64338c3d5d509a01734219e0fa473482bc0
Matt Ball <mattdrivedev@gmail.com>
2023-12-21 00:19:44 +0000

    Day04 - fixed bug (rtfm: strtok()) - Success.

--- day04/main.c
+++ day04/main.c
@@ -10,7 +10,7 @@ int main() {
 
     // It's C o'clock mofo... let's... gooooo!
     // Some of this is starting to look quite a lot more familiary now.
-    char filename[] = "test.txt";
+    char filename[] = "input.txt";
     FILE *file = fopen(filename, "r");
 
     const char delim[] = " ";
@@ -64,7 +64,10 @@ int main() {
         // once to size, and then again to populate the arrays. But, once
         // sized, we only just populate arrays.
         if (winningcount == 0) {
-            token = strtok(winningstr, delim);
+            // Clone the winningstr so that we can tokenize it.
+            char *winningstrclone = (char *)malloc(strlen(winningstr));
+            strcpy(winningstrclone, winningstr);
+            token = strtok(winningstrclone, delim);
             while (token != NULL) {
                 winningcount++;
                 token = strtok(NULL, delim);
@@ -73,7 +76,10 @@ int main() {
         }
 
         if (ourcount == 0) {
-            token = strtok(ourstr, delim);
+            // Clone the ourstr so that we can tokenize it.
+            char *ourstrclone = (char *)malloc(strlen(ourstr));
+            strcpy(ourstrclone, ourstr);
+            token = strtok(ourstrclone, delim);
             while (token != NULL) {
                 ourcount++;
                 token = strtok(NULL, delim);
@@ -82,6 +88,11 @@ int main() {
         }
 
         // TODO: There is a BUG here. The first card is not fine.
+        // Aha! strtok() is destructive. It is modifying the original
+        // string. So we need to make a copy of the string before we
+        // tokenize it. But, that happens in the above code. I'm just
+        // leaving this comment here so that I remember.
+
         printf("CARD %d\n", cardno);
         printf("   Winning numbers:\n");
         // Now we can populate the arrays with the numbers.
